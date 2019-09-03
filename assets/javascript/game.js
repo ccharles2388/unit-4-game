0ea03133ff759f6ee0c6713102394737e15589cb
Curtis Charles <ccharles2388@gmail.com>
2019-09-03 02:07:53 -0400

    completed sumScore Logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,8 +33,8 @@ var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 for (var c = 1; c < 5; c++) {
   // get a number at random from numberOptions
-  var val = Math.floor(Math.random() * numberOptions.length) +1;
- 
+  var val = Math.floor(Math.random() * numberOptions.length) + 1;
+
   console.log(val)
   $('#crystal' + c).attr('data-crystalvalue', val)
 }
@@ -48,21 +48,28 @@ $(".crystal-image").on("click", function () {
   // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
   // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
   // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
   var crystalValue = ($(this).attr("data-crystalvalue"));
   crystalValue = parseInt(crystalValue);
+
   // We then add the crystalValue to the user's "counter" which is a global variable.
   // Every click, from every crystal adds to the global counter.
   counter += crystalValue;
 
+  // Log Counter Score To Console
+  console.log(counter);
+
   // All of the same game win-lose logic applies. So the rest remains unchanged.
   alert("New score: " + counter);
 
-  if (counter === targetNumber) {
+  // Show Total Sum Of Score After Crystals Are Selected
+  $("#sumScore").text(counter);
+
+  //  If Logic To Determine If User Wins Or Loses
+  if (counter === c_randomNumber) {
     alert("You win!");
   }
 
-  else if (counter >= targetNumber) {
+  else if (counter >= c_randomNumber) {
     alert("You lose!!");
   }
 
